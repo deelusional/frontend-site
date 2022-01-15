e2d4affbd7f68e7bc731570d7582e9a4ef026b9f
dTap <imtapped@protonmail.com>
2022-01-15 15:42:55 +0700

    view filepath reordered

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const path = require('path');
 
 const express = require('express');
 
@@ -5,6 +6,11 @@ const app = express();
 
 app.get('/', function (req, res) {
   res.send('<h1>Hello Localhost:3000</h1>');
+ });
+
+app.get('/restaurants', function (req, res) {
+  const htmlFilePath = path.join(__dirname, 'views', 'restaurants.html');
+  res.sendFile(htmlFilePath)
  });
 
 
